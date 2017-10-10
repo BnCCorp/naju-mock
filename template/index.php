099283ded1fdc37639d253aa47d93364a2b218b1
Steffen B <stevnash@gmx.de>
2017-10-10 17:52:58 +0200

    add joomla template

---
+++ template/index.php
@@ -0,0 +1,29 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+<!DOCTYPE html>
+<html xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>">
+<head>
+    <jdoc:include type="head"/>
+    <header>
+        <a class="logo" title="Startseite" href="/" name="logo"><span>Startseite</span></a>
+        <div class="top-nav">
+            <jdoc:include type="modules" name="top"/>
+        </div>
+    </header>
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/style.css"
+          type="text/css"/>
+</head>
+    <body>
+        <div class="wrapper">
+            <div class="main-nav">
+                <jdoc:include type="modules" name="left"/>
+            </div>
+            <div class="content">
+                <jdoc:include type="component"/>
+                <jdoc:include type="modules" name="footer"/>
+            </div>
+            <div class="sidebar">
+                <jdoc:include type="modules" name="right"/>
+            </div>
+        </div>
+    </body>
+</html>
