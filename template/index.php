53aa456a2928cb13324e48d6f1cfe5271db5132e
Steffen B <stevnash@gmx.de>
2017-11-25 01:14:03 +0100

    Add h1, h2 and p tag style

--- template/index.php
+++ template/index.php
@@ -1,17 +1,17 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <!DOCTYPE html>
 <html xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>">
-<head>
-    <jdoc:include type="head"/>
-    <header>
-        <a class="logo" title="Startseite" href="/" name="logo"><span>Startseite</span></a>
-        <div class="top-nav">
-            <jdoc:include type="modules" name="top"/>
-        </div>
-    </header>
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/style.css"
-          type="text/css"/>
-</head>
+	<head>
+		<jdoc:include type="head"/>
+		<header>
+			<a class="logo" title="Startseite" href="/" name="logo"><span>Startseite</span></a>
+			<div class="top-nav">
+				<jdoc:include type="modules" name="top"/>
+			</div>
+		</header>
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/style.css"
+			  type="text/css"/>
+	</head>
     <body>
         <div class="wrapper">
             <div class="main-nav">
